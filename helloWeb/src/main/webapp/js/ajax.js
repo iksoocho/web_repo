a3cc72d5bb9da35a04db2a43913fd1b9f67532a9
admin <admin@DESKTOP-EEV2VM6>
2023-10-27 17:48:49 +0900

    10월27일 수업 끝

--- helloWeb/src/main/webapp/js/ajax.js
+++ helloWeb/src/main/webapp/js/ajax.js
@@ -40,6 +40,12 @@ function loadJson(){
     console.log(xhtp.responseText)
     let result = JSON.parse(xhtp.responseText);
     console.log(result)
+
+
+    let titles = ['회원번호', '비밀번호', '이름', '연락처'];
+    let tt = table.makeTable(titles, result);
+    document.getElementById('show').innerHTML = tt;
+    
 }
 
 
